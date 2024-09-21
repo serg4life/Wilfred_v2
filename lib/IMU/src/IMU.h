32863e66e3bb9f36f24692f47f52e1bd8aa0e0fa
serg4life <sergimendi2012@gmail.com>
2024-09-21 21:03:03 +0200

    Wilfred 0.1

--- lib/IMU/src/IMU.h
+++ lib/IMU/src/IMU.h
@@ -1,9 +1,4 @@
 #ifndef IMU_H
 #define IMU_H
 
-
-void displaySensorOffsets(const adafruit_bno055_offsets_t &calibData);
-void displayCalStatus(void);
-void displaySensorDetails(void);
-void displaySensorStatus(void);
 #endif
