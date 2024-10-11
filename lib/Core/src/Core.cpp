90b34f88276804a2331265081b7e41ed598b5421
serg4life <sergimendi2012@gmail.com>
2024-10-11 19:04:21 +0200

    calibration added

--- lib/Core/src/Core.cpp
+++ lib/Core/src/Core.cpp
@@ -1,5 +1,29 @@
 #include <Core.h>
 
+void displaySensorOffsets(const adafruit_bno055_offsets_t &calibData)
+{
+    Serial.print("Accelerometer: ");
+    Serial.print(calibData.accel_offset_x); Serial.print(" ");
+    Serial.print(calibData.accel_offset_y); Serial.print(" ");
+    Serial.print(calibData.accel_offset_z); Serial.print(" ");
+
+    Serial.print("\nGyro: ");
+    Serial.print(calibData.gyro_offset_x); Serial.print(" ");
+    Serial.print(calibData.gyro_offset_y); Serial.print(" ");
+    Serial.print(calibData.gyro_offset_z); Serial.print(" ");
+
+    Serial.print("\nMag: ");
+    Serial.print(calibData.mag_offset_x); Serial.print(" ");
+    Serial.print(calibData.mag_offset_y); Serial.print(" ");
+    Serial.print(calibData.mag_offset_z); Serial.print(" ");
+
+    Serial.print("\nAccel Radius: ");
+    Serial.print(calibData.accel_radius);
+
+    Serial.print("\nMag Radius: ");
+    Serial.print(calibData.mag_radius);
+}
+
 Core::Core() : 
     motor_R(MOTOR_R_PIN_A, MOTOR_R_PIN_B, 0.0),         //Initialization list, when there are objects inside other objects contructors.
     motor_L(MOTOR_L_PIN_A, MOTOR_L_PIN_B, 0.0),
@@ -26,6 +50,7 @@ void Core::initIMU(void){
     }
     bno.setAxisRemap(Adafruit_BNO055::REMAP_CONFIG_P1);     //remap, X points forward
     bno.setMode(OPERATION_MODE_NDOF);
+    bno.setExtCrystalUse(true);
     bno.enterSuspendMode();
 };
 
@@ -39,13 +64,12 @@ void Core::sleepIMU(void){
 
 void Core::calibrateIMU(void){
     sensors_event_t event;
-    bno.setExtCrystalUse(true);
-    while(!bno.isFullyCalibrated()){
+    uint8_t system, gyro, accel, mag = 0;
+    while(!(system == 3 && gyro == 3 && mag == 3)){
         bno.getEvent(&event);
         /* Get the four calibration values (0..3) */
         /* Any sensor data reporting 0 should be ignored, */
         /* 3 means 'fully calibrated" */
-        uint8_t system, gyro, accel, mag = 0;
         bno.getCalibration(&system, &gyro, &accel, &mag);
 
         /* Display the individual values */
@@ -60,6 +84,13 @@ void Core::calibrateIMU(void){
 
         delay(BNO055_SAMPLERATE_DELAY_MS);
     }
+    Serial.println("\nFully calibrated!");
+    Serial.println("--------------------------------");
+    Serial.println("Calibration Results: ");
+    adafruit_bno055_offsets_t newCalib;
+    bno.getSensorOffsets(newCalib);
+    displaySensorOffsets(newCalib);
+    bno.setSensorOffsets(newCalib);
 };
 
 bool Core::areMotorsEnabled(void){
