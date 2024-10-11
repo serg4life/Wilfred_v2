2c85626e61e2653d0ad7f3af78cd5aefc215103a
serg4life <sergimendi2012@gmail.com>
2024-10-11 20:11:19 +0200

    calibrate button implemented and handler updated

--- lib/Controller/src/Controller.h
+++ lib/Controller/src/Controller.h
@@ -9,8 +9,6 @@
 #include <SPIFFS.h>
 #include <Core.h>
 
-typedef enum {FORWARD_CMD, BACKWARD_CMD, STOP_CMD, ENABLE_MOTORS_CMD} Commands;
-
 class WebController {
     public:
         WebController(Core param_core);
